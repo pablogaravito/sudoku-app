9b7793d72d446e68b61fad1e9754e308111479a6
Pablo <pablo.garavito@gmail.com>
2026-05-26 14:46:58 -0500

    fixing gh pages config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/sudoku-app/",
   server: {
     port: 3000,
     strictPort: true, // don't try another port automatically
